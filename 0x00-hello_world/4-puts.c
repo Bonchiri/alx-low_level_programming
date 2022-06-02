55c30c8366e57fcc088a794f84ab7ff379af0b18
Brian Onchiri <brionogega02@gmail.com>
2022-06-02 05:31:23 -0700

    Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
    Use the function puts
    
    You are not allowed to use printf
    
    Your program should end with the value 0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- *
- *main - entry point
- *@void: no arguements
- *Description: the program starts and ends here.
- *Return: value 0 to exit
- */
+* main - entry point
+* @void: no arguements
+* Description: the program starts and ends here
+*Return: value 0 to exit
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
