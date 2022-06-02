a8ed894b75674390f667de8bcb9c104e6ef2b95f
Brian Onchiri <brionogega02@gmail.com>
2022-06-02 05:42:46 -0700

    Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.
    Use the function printf
    
    You are not allowed to use the function puts
    
    Your program should return 0
    
    Your program should compile without warning when using the -Wall gcc option

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
-*main -entry point
-*@void: no arguements
-*Description: the program starts and ends here
-*Return: value 0 to exit 
+* main -entry point
+* @void: no arguements
+* Description: the program starts and ends here
+* Return: value 0 to exit 
 */
 int main(void)
 {
