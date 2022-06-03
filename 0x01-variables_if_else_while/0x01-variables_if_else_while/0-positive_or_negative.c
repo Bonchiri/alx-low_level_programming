2177496d7759612ca3e6cb1a8247b7aa12c79d98
Brian Onchiri <brionogega02@gmail.com>
2022-06-03 01:37:02 -0700

    0. Positive anything is better than negative nothing

---
+++ 0x01-variables_if_else_while/0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+* 
+* Return: always 0 (Success/correct)
+*/
+
+int main(void)
+{
+int n;
+
+srand(time(0));
