777b15a20543d4a6dc2f4adef2aa0ac97b156416
Brian Onchiri <brionogega02@gmail.com>
2022-06-27 02:14:36 -0700

    task 3

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,15 +1,18 @@
 #include <stdlib.h>
+
 /**
 * str_concat - function that concatenates two strings.
 * @s1: input char 1
 * @s2: input char 2
 * Return char
 */
+
 char *str_concat(char *s1, char *s2)
 {
 char *cat;
 char *nul = "";
 unsigned int i, j, x, y;
+
 i = j = x = y = 0;
 if (s1 == NULL)
 s1 = nul;
